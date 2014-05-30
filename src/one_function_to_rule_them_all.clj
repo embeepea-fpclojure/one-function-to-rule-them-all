70889686c5afff7931c00982eac8045f675eafa8
Mark Phillips <embeepea@git>
2014-05-29 23:03:15 -0400

    without encore problem

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,43 +1,74 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq)
+    ""
+    (reduce
+     (fn [a s] (str a " " s))
+     a-seq)))
+     
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (cond
+   (empty? a-seq)        '()
+   (empty? (rest a-seq)) (apply list a-seq)
+   :else (reduce
+                (fn [acc elem] (conj (conj acc x) elem))
+                [(first a-seq)]
+                (rest a-seq))))
 
 (defn my-count [a-seq]
-  :-)
+  (reduce (fn [acc s] (inc acc)) 0 a-seq))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce (fn [acc s] (cons s acc)) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+    (reduce (fn [[mn mx] e] [(min mn e) (max mx e)])
+            [(first a-seq) (first a-seq)]
+            (rest a-seq)))
+          
 
 (defn insert [sorted-seq n]
-  [:-])
+  (cond
+   (empty? sorted-seq)            (list n)
+   (< n (first sorted-seq))       (cons n sorted-seq)
+   :else (cons (first sorted-seq) (insert (rest sorted-seq) n))))
 
 (defn insertion-sort [a-seq]
-  [:-])
-
+  (reduce insert '() a-seq))
+          
 (defn parity [a-seq]
-  [:-])
+  (let [toggle (fn [a-set elem]
+                 (if (contains? a-set elem)
+                   (disj a-set elem)
+                   (conj a-set elem)))]
+    (reduce (fn [st e] (toggle st e))
+            #{}
+            a-seq)))
 
-(defn minus [x]
-  :-)
+(defn minus
+  ([x] (- x))
+  ([x y] (- x y)))
 
-(defn count-params [x]
-  :-)
+(defn count-params [& more]
+  (count more))
 
-(defn my-* [x]
-  :-)
+(defn my-*
+  ([]         1)
+  ([x]        x)
+  ([x & more] (reduce * x more)))
 
-(defn pred-and [x]
-  (fn [x] :-))
+(defn pred-and
+  ([]             (fn [&more] true))
+  ([pred]         pred)
+  ([pred & preds] (reduce (fn [acc p] (fn [x] (and (acc x) (p x))))
+                          pred
+                          preds)))
+
 
 (defn my-map [f a-seq]
   [:-])
